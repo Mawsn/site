904fd10a18825f7840830b030e6cfa7b6a665cea
Mawsn <54127173+Mawsn@users.noreply.github.com>
2021-04-14 15:43:01 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-self.process.addListener('fetch', function(event){});
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
+workbox.routing.registerRoute(
+	({request}) => request.destination == 'image',
+	new workbox.strategies.CacheFirst()
+);
